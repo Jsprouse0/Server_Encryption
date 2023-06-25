81929afe4a8ab4f82336fc1861616d8e57b144d3
Jsprouse0 <Jacobmsprouse@gmail.com>
2023-06-25 10:40:53 -0500

    Server and Client Encryption using Python

--- Client.py
+++ Client.py
@@ -1,27 +1,42 @@
-import socket                                                                       # Robert Gleason and Jacob Sprouse
+# Robert Gleason and Jacob Sprouse
+import socket
 from Server import Socket
+from Cryptodome.Cipher import AES
+from Cryptodome.Random import get_random_bytes
+from Cryptodome.Util.Padding import pad, unpad
 
 # create socket object
 connectionSocket = Socket.server()
 host = Socket.host()
 port = Socket.port()
-connection = True
 
+key = get_random_bytes(16)
+connection = True
+connectionSocket.connect((host, port))
 
 while connection:
-    connectionSocket.connect((host, port))
-
-    message = 'hello I am the client'
-
+    # Client message
+    message = input()
     message_encode = message.encode()
 
-    connectionSocket.send(message_encode)
+    # encryption key
+    encrypt_cipher = AES.new(key, AES.MODE_ECB)
+    ciphertext = encrypt_cipher.encrypt(pad(message_encode, AES.block_size))
+    print(ciphertext)
 
-    received_bytes = connectionSocket.recv(1024)
+    # sends message across socket to server
+    connectionSocket.send(ciphertext)
+    connectionSocket.send(key)
 
-    received_message = bytes.decode(received_bytes)
+    # Server Response
+    received_ciphertext = connectionSocket.recv(1024)
+    decrypt_cipher = AES.new(key, AES.MODE_ECB)
+    decrypt_bytes = unpad(decrypt_cipher.decrypt(received_ciphertext), AES.block_size)
 
-    print(received_message)
-    connection == False
+    received_message = bytes.decode(decrypt_bytes)
 
-connectionSocket.close()
+    print(f"The cipher text is {received_ciphertext} and the message is {received_message}")
+
+    if received_message == 'Bye' or received_message == 'bye':
+        connectionSocket.close()
+        connection = False
--- Server.py
+++ Server.py
@@ -1,4 +1,9 @@
 import socket                                                                   # Robert Gleason and Jacob Sprouse
+from Cryptodome.Cipher import AES
+from Cryptodome.Random import get_random_bytes
+from Cryptodome.Util.Padding import pad, unpad
+
+# excess receiving overloads the server, try and fix
 
 
 class Socket(object):
@@ -33,27 +38,39 @@ if __name__ == '__main__':
 
     # start listening
     server_socket.listen()
+    print("Waiting for connection.....")
+
+    # addr_port is a tuple that contains both the address and the port number
+    # clientSocket is the communication variable between host and port
+    client_socket, addr_port = server_socket.accept()
+
+    print("\nGot a connection from " + str(addr_port))
 
     while True:
-        print("Waiting for connection.....")
-
-        # addr_port is a tuple that contains both the address and the port number
-        # clientSocket is the communication variable between host and port
-        client_socket, addr_port = server_socket.accept()
-
-        print("\nGot a connection from " + str(addr_port))
-
+        # Client response
         received_bytes = client_socket.recv(1024)
 
-        received_message = bytes.decode(received_bytes)
+        # decryption key
+        received_key = client_socket.recv(1024)
+        decrypt_cipher = AES.new(received_key, AES.MODE_ECB)
+        ciphertext = unpad(decrypt_cipher.decrypt(received_bytes), AES.block_size)
 
-        print(received_message)
+        received_message = bytes.decode(ciphertext)
+        print(f"The cipher text is {received_message}")
 
-        # Response
-        message = 'hi'
-
+        # Server message
+        message = input()
         message_encode = message.encode()
 
-        client_socket.send(message_encode)
+        # encryption
+        encrypt_cipher = AES.new(received_key, AES.MODE_ECB)
+        server_cipher_text = encrypt_cipher.encrypt(pad(message_encode, AES.block_size))
 
-        client_socket.close()
+        client_socket.send(server_cipher_text)
+
+        if received_message == 'Bye' or received_message == 'bye':
+            client_socket.close()
+            print('Back to listening...')
+            server_socket.listen()
+            client_socket, addr_port = server_socket.accept()
+
